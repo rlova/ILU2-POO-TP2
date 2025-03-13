2a4fb33d3380ea085481c3bd1ecaf9885df6da18
Mbolomihaja Remano lova <rmm4917a@ensfsi.onmicrosoft.com>
2025-03-13 19:46:35 +0100

    boundary emmenager

--- src/frontiere/BoundaryEmmenager.java
+++ src/frontiere/BoundaryEmmenager.java
@@ -27,7 +27,10 @@ public class BoundaryEmmenager {
 					break;
 
 				case 2:
-					//TODO a completer
+					System.out.println("Bienvenue villageois "+ nomVisiteur);
+					question.append(" Quelle est votre force ?");
+					int force = Clavier.entrerEntier(question.toString());
+					controlEmmenager.ajouterGaulois(nomVisiteur,force);
 					break;
 
 				default:
@@ -40,6 +43,13 @@ public class BoundaryEmmenager {
 	}
 
 	private void emmenagerDruide(String nomVisiteur) {
-		//TODO a completer
+		StringBuilder question = new StringBuilder();
+		question.append("Quelle est la force de potion la plus faible que vous produisez ?");
+		int effetPotionMin = Clavier.entrerEntier(question.toString());
+		question.append("Quelle est la force de potion la plus forte que vous produisez ?");
+		int effetPotionMax = Clavier.entrerEntier(question.toString());
+//		if (effetPotionMax < effetPotionMin) {
+//			System.out.println("Attention Druide, vous vous etres trompé dans le minimum et le maximum");
+//		}
 	}
 }
