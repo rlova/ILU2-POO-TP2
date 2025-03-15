f64f2f5d4458eb31135e1042dbcc597033c04161
rlova <lovaremano@gmail.com>
2025-03-15 21:16:27 +0100

    modification PrendreEtal

--- src/frontiere/BoundaryPrendreEtal.java
+++ src/frontiere/BoundaryPrendreEtal.java
@@ -12,12 +12,12 @@ public class BoundaryPrendreEtal {
 	public void prendreEtal(String nomVendeur) {
 		boolean nomVendeurConnu = controlPrendreEtal.verifierIdentite(nomVendeur);
 		if (!nomVendeurConnu) {
-			System.out.println("Je suis désolé "+nomVendeur+" mais il faut etre un habitant de notre village pour commencer ici.\n");
+			System.out.println("Je suis désolé "+nomVendeur+" mais il faut etre un habitant de notre village pour commencer ici.");
 		} else {
-			System.out.println("Bonjour "+nomVendeur+" , je vaus regarder su je peux vous trouver un étal.\n");
+			System.out.println("Bonjour "+nomVendeur+" , je vaus regarder su je peux vous trouver un étal.");
 			boolean etalDisponible = controlPrendreEtal.resteEtals();
 			if (!etalDisponible) {
-				System.out.println("Désolé "+nomVendeur+" je n'ai plus d'étal qui ne soit pas déjà occupé.\n");
+				System.out.println("Désolé "+nomVendeur+" je n'ai plus d'étal qui ne soit pas déjà occupé.");
 			} else {
 				installerVendeur(nomVendeur);
 			}
@@ -26,11 +26,12 @@ public class BoundaryPrendreEtal {
 
 	private void installerVendeur(String nomVendeur) {
 		StringBuilder question = new StringBuilder();
-		System.out.println("C'est parfait, il me reste un étal pour vous !\n");
-		System.out.println("Il me faudrait quelques renseignements :\n");
-		question.append("Quel produit souhiatez-vous vendre ?\n");
+		System.out.println("C'est parfait, il me reste un étal pour vous !");
+		System.out.println("Il me faudrait quelques renseignements :");
+		question.append("Quel produit souhaitez-vous vendre ?");
 		String produit = Clavier.entrerChaine(question.toString());
 		question = new StringBuilder();
+		question.append("Combien souhaitez-vous en vendre ?");
 		int nbProduit = Clavier.entrerEntier(question.toString());
 		int numeroEtal = controlPrendreEtal.prendreEtal(nomVendeur, produit, nbProduit);
 		if (numeroEtal!=-1) {
