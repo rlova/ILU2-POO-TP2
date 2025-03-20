71e3acd39e10da5bdb4b2359e727eff7d9aab3f1
Mbolomihaja Remano lova <rmm4917a@ensfsi.onmicrosoft.com>
2025-03-20 19:05:56 +0100

    ControlLibererEtal

--- src/controleur/ControlLibererEtal.java
+++ src/controleur/ControlLibererEtal.java
@@ -1,5 +1,7 @@
 package controleur;
 
+import villagegaulois.Etal;
+
 public class ControlLibererEtal {
 	private ControlTrouverEtalVendeur controlTrouverEtalVendeur;
 
@@ -21,8 +23,22 @@ public class ControlLibererEtal {
 	 * 		[4] : quantité de produit vendu
 	 */
 	public String[] libererEtal(String nomVendeur) {
-		//TODO a completer
+		Etal etal = controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur);
+		boolean vendeurReconnu = etal != null;
 		String[] donneesEtal = null;
+		if (!vendeurReconnu) {
+			System.out.println("Mais vous n'etes pas inscrit sur notre marche aujourd'hui !");
+		} else {
+			donneesEtal = etal.etatEtal();
+			String etalOccupe = donneesEtal[0];
+			String produit = donneesEtal[1];
+			String quantiteInitial = donneesEtal[2];
+			String quantiteVendu = donneesEtal[3];
+			if (etalOccupe!="true") {
+				System.out.println("Vous avez vendu "+ quantiteVendu + " sur "+quantiteInitial+" "+produit+".");
+				System.out.println("Au revoir "+nomVendeur+", passez une bonne journee");
+			}
+		}
 		return donneesEtal;
 	}
 
