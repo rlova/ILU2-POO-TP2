de7316b07aa533c6623d9b6822573906d7062c68
rlova <lovaremano@gmail.com>
2025-04-09 19:13:21 +0200

    modif test afficherVillage

--- test/controleur/ControlAfficherVillageTest.java
+++ test/controleur/ControlAfficherVillageTest.java
@@ -11,8 +11,8 @@ import personnages.Gaulois;
 import villagegaulois.Village;
 
 class ControlAfficherVillageTest {
-	private ControlAfficherVillage controlAfficherVillage;
-	private Village village;
+	private ControlAfficherVillage controlAfficherVillage, controlAfficherVillageDuChef;
+	private Village village, villageDuChef;
 	private Chef chef;
 	private Gaulois gaulois;
 	private Druide druide;
@@ -21,13 +21,13 @@ class ControlAfficherVillageTest {
 	public void initialiserSituation() {
 		village = new Village("le village", 5, 2);
 		controlAfficherVillage = new ControlAfficherVillage(village);
-		chef = new Chef("le chef", 5, village);
+		chef = new Chef("chef", 5, village);
 		village.setChef(chef);
 		village.getNom();
-		gaulois = new Gaulois("Le gaulois 1", 7);
+		gaulois = new Gaulois("gaulois", 7);
 		druide = new Druide("druide", 5, 6, 12);
 		village.ajouterHabitant(gaulois);
-		village.ajouterHabitant(druide);
+		village.ajouterHabitant(druide);		
 	}
 
 	@Test
@@ -40,6 +40,18 @@ class ControlAfficherVillageTest {
 		String[] noms = controlAfficherVillage.donnerNomsVillageois();
 		assertNotNull(noms, "La liste des noms ne doit pas être null");
 		assertEquals(3, noms.length, "Il doit y avoir 3 villageois (le chef, le druide et le gaulois");
+		assertEquals("chef",noms[0]);	
+		assertEquals("gaulois",noms[1]);
+		assertEquals("le druide druide",noms[2]);
+		
+		// test sur un village sans chef
+		villageDuChef = new Village("village du chef", 4, 2);
+		controlAfficherVillageDuChef = new ControlAfficherVillage(villageDuChef);
+		Chef ChefSeulHabitant = new Chef("solo", 4, villageDuChef);
+		villageDuChef.setChef(ChefSeulHabitant);
+		String[] nomDuChef = controlAfficherVillageDuChef.donnerNomsVillageois();
+		assertEquals(1, nomDuChef.length,"le tableau de String doit contenir seulement le nom du chef");
+		assertEquals("solo",nomDuChef[0]);
 	}
 
 	@Test
